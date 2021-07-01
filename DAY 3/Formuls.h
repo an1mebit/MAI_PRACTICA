e10af5d89f002a0104b612034cb13d4f08b91d8f
an1mebit <73356718+an1mebit@users.noreply.github.com>
2021-07-01 13:37:22 +0300

    Create Formuls.h
    Added variable values as a as a header file

---
+++ DAY 3/Formuls.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+
+float V = 0;
+float Vx = 0;
+float Vy = 0;
+float g = 9.8;
+float h = 0;
+float hmax = 0;
+const float R = 0.003;
+float dt = 0.001;
+float t = 0;
+float x = 0;
+float y = 0;
+float m = 0;
+float yk = 0;
+float xk = 0;
+float Fx = 0;
+float Fy = 0;
+float ay = 0;
+float ax = 0;
+float a;
